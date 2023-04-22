fe2ede600899df9f912058cd1d5efe712efa0321
IreneMercy <irenemercy700@gmail.com>
2023-04-22 13:28:54 -0700

    fixed betty errors and refractored code

--- _printf.c
+++ _printf.c
@@ -1,89 +1,100 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
-
-#define BUFF_SIZE 1024
+#include "main.h"
 
 /**
- * buffering - writes a buffer to stdoutput
- * @buffer: the buffer to write
- * @buff: pointer to the buffer index
+ * print_char - Print a single character.
+ * @arg: The character to print.
+ * Return: The number of characters printed (always 1).
  */
-void buffering(char buffer[], int *buff)
+int print_char(va_list arg)
 {
-	if (*buff > 0)
-		/* write content of buffer*/
-		write(1, &buffer[0], *buff);
+	char c = va_arg(arg, int);
 
-	/* clear buffer so can be reused*/
-	*buff = 0;
+	write(1, &c, 1);
+	return (1);
 }
+
 /**
- * _printf - custom printf implementation
- * @format: format string
- * @...: vardiac arguments
- * Return: number of characters printed
+ * print_string - Print a string.
+ * @arg: The string to print.
+ * Return: The number of characters printed.
+ */
+int print_string(va_list arg)
+{
+	char *s = va_arg(arg, char *);
+	int len = 0;
+
+	if (s == NULL)
+		s = "(null)";
+
+	while (*s != '\0')
+	{
+		write(1, s, 1);
+		s++;
+		len++;
+	}
+
+	return (len);
+}
+
+/**
+ * print_percent - Print a percent sign.
+ * @arg: Unused.
+ * Return: The number of characters printed (always 1).
+ */
+int print_percent(va_list arg)
+{
+	(void)arg;
+	write(1, "%", 1);
+	return (1);
+}
+
+/**
+ * _printf - Print formatted output to stdout.
+ * @format: The format string.
+ * Return: The number of characters printed.
  */
 int _printf(const char *format, ...)
 {
-	int i, j, characters_printed = 0, buff = 0;
-	char buffer[BUFF_SIZE];
-	va_list args_list;
+	va_list arg;
+	const char *p;
+	int len = 0;
 
-	va_start(args_list, format);
+	va_start(arg, format);
 
-	/* loop through each character in format */
-	for (i = 0; format && format[i]; i++)
+	for (p = format; *p != '\0'; p++)
 	{
-		/* if current character is not '%' add it to buffer */
-		if (format[i] != '%')
+		if (*p == '%')
 		{
-			buffer[buff++] = format[i];
+			p++;
 
-			/* if buffer is full write it to sdoutput */
-			if (buff == BUFF_SIZE)
-				buffering(buffer, &buff);
-			/* increment characters printed */
-			characters_printed++;
+			switch (*p)
+			{
+				case 'c':
+					len += print_char(arg);
+					break;
+				case 's':
+					len += print_string(arg);
+					break;
+				case '%':
+					len += print_percent(arg);
+					break;
+				default:
+					write(1, "%", 1);
+					write(1, &(*p), 1);
+					len += 2;
+			}
 		}
 		else
 		{
-			buffering(buffer, &buff);
-			i++;
-			switch (format[i])
-			{
-				case 'c':
-					j = va_arg(args_list, int);
-					buffer[buff++] = (char)j;
-
-					if (buff == BUFF_SIZE)
-						buffering(buffer, &buff);
-					characters_printed++;
-					break;
-
-				case 's':
-					for (j = 0; format[i] && format[i] != '%'; i++, j++)
-					{
-					buffer[buff++] = format[i];
-					if (buff == BUFF_SIZE)
-						buffering(buffer, &buff);
-					buffering(buffer, &buff);
-					}
-					i--;
-					characters_printed += j;
-					break;
-
-				default:
-					buffer[buff++] = '%';
-					if (buff == BUFF_SIZE)
-						buffering(buffer, &buff);
-					buffer[buff++] = format[i];
-					characters_printed += 2;
-					break;
-			}
+			write(1, &(*p), 1);
+			len++;
 		}
 	}
-	buffering(buffer, &buff);
-	return (characters_printed);
-	va_end(args_list);
+
+	va_end(arg);
+
+	return (len);
 }
