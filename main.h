ec3613a3fcd813e824b6aea8eada4df127a73e9e
IreneMercy <irenemercy700@gmail.com>
2023-04-23 06:32:01 -0700

    add conversion prototype to main.h

--- main.h
+++ main.h
@@ -2,4 +2,8 @@
 #define HEADER_FILE
 
 int _printf(const char *format, ...);
+int print_char(va_list arg);
+int print_string(va_list arg);
+int print_percent(va_list arg);
+int print_integer(va_list arg);
 #endif
